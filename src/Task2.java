372aee78978568e2e84d510637add70fe273a8a3
Nikolay Rusanov <nikolarusano92@gmail.com>
2021-05-16 23:32:29 +0300

    Task3_start

--- src/Task2.java
+++ src/Task2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task2 {
+public class Task2<quantityworkHours> {
     //Расчёт зарплаты агента по продажам
     public static void main(String[] args) {
         Scanner scanner = new Scanner((System.in));
@@ -8,48 +8,70 @@ public class Task2 {
                 "work hours:");
         int quantityWorkHours = scanner.nextInt();
         System.out.println("work rate employee");
-        double basicRate = scanner.nextDouble();
+        double basicRate = (double) scanner.nextDouble();
         System.out.println("Input work experience:");
         int workExperience = scanner.nextInt();
-
+        System.out.println("Input quantity sales:");
+        int quantitySales = scanner.nextInt();
+        System.out.println("Input Amount sales:");
+        double salesAmount = scanner.nextDouble();
+        System.out.println("The total amount of salary is:");
+        double salary = CountBasicSalary(quantityWorkHours,basicRate) + CountSalaryByWorkHours(quantityWorkHours,basicRate) +
+                CountSalaryByWorkHoursAndWorkExperience(quantityWorkHours,basicRate,workExperience) + CountSalaryByQuantitySales(quantitySales)+
+                CountSalaryByAmountSales(salesAmount);
+        System.out.println(salary);
     }
 
-    static double CountSalary(int quantityworkHours, double basicRate) {
+    static double CountBasicSalary(int quantityworkHours, double basicRate) {
         double salary;
-        return salary = quantityworkHours * basicRate;
+        return salary = 160 * basicRate;
     }
 
 
-    static double CountSalaryByWorkHours(salary, double basicRate) {
+    static double CountSalaryByWorkHours(int quantityworkHours, double basicRate) {
         double salarybonus;
-        if (quantityworkHours >= 0 & quantityworkHours <= 160) {
-            return salarybonus = 1;
 
-        } else if (quantityworkHours > 160) {
-            return salarybonus = CountSalary(quantityworkHours - 160, basicRate) * 1.5;
+        if (quantityworkHours > 160) {
+            return salarybonus = ((quantityworkHours - 160)*basicRate) * 1.5;
         } else {
-            System.out.println("Incorrect input");
+            //System.out.println("The person doesn't get overtime bonus");
             return 0;
         }
     }
 
     static double CountSalaryByWorkHoursAndWorkExperience(int quantityworkHours, double basicRate, int workExperience) {
-        double salary;
-        if (quantityworkHours >= 0 & quantityworkHours <= 160) {
-            if (workExperience <= 2) {
-                return salary = CountSalary(quantityworkHours, basicRate) * 1;
-            } else if (workExperience > 2 & workExperience <= 4) {
-                return salary = CountSalary(quantityworkHours, basicRate) * 1.2;
-            } else if (workExperience > 4 & workExperience <= 6) {
-                return salary = CountSalary(quantityworkHours, basicRate) * 1.2;
-            } else if (workExperience > 6) {
-                return salary = CountSalary(quantityworkHours, basicRate) * 1.2;
-            }
+        double salaryBonus;
+        switch (workExperience){
+            case 0:
+            case 1:
+            case 2:return salaryBonus = CountBasicSalary(quantityworkHours,basicRate) *.01;
+
+            case 3:
+            case 4:return salaryBonus = CountBasicSalary(quantityworkHours,basicRate) *.012;
+            case 5:
+            case 6:return salaryBonus = CountBasicSalary(quantityworkHours,basicRate) *.013;
+            default:return salaryBonus = CountBasicSalary(quantityworkHours,basicRate) *.014;
+
         }
     }
 
-    static double CountSalaryByQuantitySales(int quantityworkHours, double basicRate, int workExperience){
-
+    static double CountSalaryByQuantitySales(int quantitySales){
+        double salaryBonus;
+        if(quantitySales > 20){
+            return salaryBonus = 250.0;
+        }else if(quantitySales < 10){
+            return salaryBonus = -150.0;
+        }else {
+            return 0;
+        }
 }
+    static double CountSalaryByAmountSales(double salesAmount){
+        double salaryBonus;
+        if(salesAmount > 15000){
+            return salaryBonus = 1000.0;
+        }else{
+            return 0;
+        }
+    }
 
 }
---
+++ src/Task3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input used GB internet:");
+        double usedGB = scanner.nextDouble();
+        System.out.println("input minutes of talk by phone:");
+        double minutesPhoneTalk = scanner.nextDouble();
+        System.out.println("Input quantity sent sms messages:");
+        int quantitySentSms = scanner.nextInt();
+        System.out.println("Input abroad calls zone1:");
+        double abroadCallsZone1 = scanner.nextDouble();
+        System.out.println("Input abroad calls zone2:");
+        double abroadCallsZone2 = scanner.nextDouble();
+    }
+
+    static
+}
